f0f66c74b089b0803e236894d504fe267a76a314
光明与尘埃 <guangmingyuchenai@163.com>
2026-07-08 16:38:35 +0800

    style:移除天气显示并将时间显示居中

--- src/views/Func/index.vue
+++ src/views/Func/index.vue
@@ -21,7 +21,7 @@
               <span> {{ currentTime.hour }}:{{ currentTime.minute }}:{{ currentTime.second }}</span>
             </div>
           </div>
-          <Weather />
+          <!-- <Weather /> -->
         </div>
       </el-col>
     </el-row>
@@ -33,7 +33,7 @@ import { getCurrentTime } from "@/utils/getTime";
 import { mainStore } from "@/store";
 import Music from "@/components/Music.vue";
 import Hitokoto from "@/components/Hitokoto.vue";
-import Weather from "@/components/Weather.vue";
+// import Weather from "@/components/Weather.vue";
 
 const store = mainStore();
 
@@ -111,7 +111,7 @@ onBeforeUnmount(() => {
       display: flex;
       flex-direction: column;
       align-items: center;
-      justify-content: space-between;
+      justify-content: center;
       animation: fade 0.5s;
       .time {
         font-size: 1.1rem;
